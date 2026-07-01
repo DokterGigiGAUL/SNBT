e4a3d5d1f894d2f67979b270b1f8c596b2c37e90
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-02 00:21:17 +0700

    Update users.js

--- data/users.js
+++ data/users.js
@@ -1,1 +1,12 @@
-
+const users = [
+    {
+        id: 1,
+        nama: "Afwan",
+        target: "Teknik Elektro ITB"
+    },
+    {
+        id: 2,
+        nama: "Athar",
+        target: "FKG Unpad"
+    }
+];
