949949022a936cd8d10619100b556ebef2988e68
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-02 00:39:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
+const userList = document.getElementById("userList");
+
+// Membuat daftar akun dari users.js
+users.forEach((user, index) => {
+
+    const label = document.createElement("label");
+    label.className = "user-option";
+
+    label.innerHTML = `
+        <input
+            type="radio"
+            name="user"
+            value="${user.id}"
+            ${index === 0 ? "checked" : ""}
+        >
+
+        <span>${user.nama}</span>
+        <small>Target: ${user.target}</small>
+    `;
+
+    userList.appendChild(label);
+
+});
+
+// Login
 document.getElementById("loginForm").addEventListener("submit", function (e) {
 
     e.preventDefault();
 
-    const selectedUser = document.querySelector('input[name="user"]:checked').value;
+    const selectedUser =
+        document.querySelector('input[name="user"]:checked').value;
 
     localStorage.setItem("activeUser", selectedUser);
 
