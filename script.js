f70767fd40d97b90899ed7a8410708e97a6e601c
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-02 19:02:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,5 @@
-const userList = document.getElementById("userList");
-
-// Membuat daftar akun dari users.js
-users.forEach((user, index) => {
-
-    const label = document.createElement("label");
-    label.className = "user-option";
-
-    label.innerHTML = `
-    <input
-        type="radio"
-        name="user"
-        value="${user.id}"
-        ${index === 0 ? "checked" : ""}
-    >
-
-    <div class="user-info">
-        <span style="line-height:1.2;">${user.nama}</span>
-
-        <small>Pilihan fakultas: <input
-            type="text"
-            class="target-input"
-            id="target-${user.id}"
-            value="${user.target}"
-        >
-        </small>
-    </div>
-`;
-
-    userList.appendChild(label);
-
-});
-
-// Login
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-
+document.getElementById("loginForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const selectedUser =
-    document.querySelector('input[name="user"]:checked').value;
-
-const target =
-    document.getElementById(`target-${selectedUser}`).value;
-
-localStorage.setItem("activeUser", selectedUser);
-localStorage.setItem(`target_${selectedUser}`, target);
-
-window.location.href = "pages/dashboard.html";
-
+    console.log("Tombol Masuk diklik");
 });
