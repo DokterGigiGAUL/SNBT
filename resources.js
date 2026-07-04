8fd2c4557b9875fafa702b8c32e1d624f1f6fbf8
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-04 18:38:07 +0700

    Update resources.js

--- resources.js
+++ resources.js
@@ -1,1 +1,199 @@
+const resources = {
 
+    "PU-L01": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-L02": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-L03": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-L04": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-L05": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-L06": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-L07": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-L08": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-L09": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-A01": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-A02": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-A03": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-A04": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-A05": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-A06": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-A07": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-A08": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-A09": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-D01": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-D02": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-D03": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-D04": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-D05": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-D06": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-D07": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-I01": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-I02": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-I03": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-I04": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-I05": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-I06": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-I07": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
+
+    "PU-I08": {
+        ringkasan: "",
+        tips: "",
+        contoh: ""
+    },
