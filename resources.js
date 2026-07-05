7763ea3157be8f08617de754b86e641bc105fef3
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-05 18:09:52 +0700

    Update resources.js

--- resources.js
+++ resources.js
@@ -1,200 +1,178 @@
 const resources = {
 
-    "PU-L01": {
-        ringkasan: "https://docs.google.com/document/d/e/2PACX-1vSq3-Xfhx5EWTqoVWoAAs0wk8jLcDikhewF_CcLXdbHQJL1V_iZGUWJsUHMN5d7HliXGl1sUMRUZLqA/pub",
-        tips: "https://docs.google.com/document/d/e/2PACX-1vTH9CWAMILa8LMRjJUFEVVUvmEfBcoPbGxTxhyY-BiMcvKy9kYpoi18wHcVZbzlqKmjtE7UhgUTbOPz/pub",
-        contoh: "https://docs.google.com/document/d/e/2PACX-1vS3OCvxJF2SqoIckMjIsYlxmzrpmuFq7omUbYwLRiJuZzuQLwbmMXix2cVU3Kw_pWMf-PohvY14lOM9/pub"
-    },
+    // =====================================================
+    // TPS - Kemampuan Penalaran Umum
+    // =====================================================
 
-    "PU-L02": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-KPU-A01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-KPU-A02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-KPU-A03": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-KPU-A04": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-L03": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-KPU-B01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-KPU-B02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-KPU-B03": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-KPU-B04": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-KPU-B05": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-L04": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-KPU-C01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-KPU-C02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-KPU-C03": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-L05": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    // =====================================================
+    // TPS - PPU
+    // =====================================================
 
-    "PU-L06": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-PPU-A01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PPU-A02": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-L07": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-PPU-B01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PPU-B02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PPU-B03": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PPU-B04": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-L08": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-PPU-C01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PPU-C02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PPU-C03": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PPU-C04": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PPU-C05": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PPU-C06": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-L09": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    // =====================================================
+    // TPS - PBM
+    // =====================================================
 
-    "PU-A01": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-PBM-A01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-A02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-A03": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-A02": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-PBM-B01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-B02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-B03": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-B04": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-B05": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-A03": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-PBM-C01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-C02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-C03": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-C04": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-C05": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PBM-C06": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-A04": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    // =====================================================
+    // TPS - Pengetahuan Kuantitatif
+    // =====================================================
 
-    "PU-A05": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-PK-A01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A03": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A04": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A05": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A06": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A07": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A08": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A09": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A10": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A11": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A12": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-A13": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-A06": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-PK-B01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-B02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-B03": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-B04": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-B05": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-B06": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-B07": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-A07": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-PK-C01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-C02": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-C03": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-C04": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-C05": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-C06": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-C07": { ringkasan:"", tips:"", contoh:""" },
 
-    "PU-A08": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "TPS-PK-D01": { ringkasan:"", tips:"", contoh:"" },
+    "TPS-PK-D02": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-A09": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    // =====================================================
+    // Literasi Bahasa Indonesia
+    // =====================================================
 
-    "PU-D01": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "LBI-A01": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-A02": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-A03": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-A04": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-D02": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "LBI-B01": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-B02": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-B03": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-B04": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-B05": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-D03": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "LBI-C01": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-C02": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-C03": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-C04": { ringkasan:"", tips:"", contoh:"" },
+    "LBI-C05": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-D04": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    // =====================================================
+    // Literasi Bahasa Inggris
+    // =====================================================
 
-    "PU-D05": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "LBE-A01": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-A02": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-A03": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-A04": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-D06": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "LBE-B01": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-B02": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-B03": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-B04": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-B05": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-D07": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "LBE-C01": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-C02": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-C03": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-C04": { ringkasan:"", tips:"", contoh:"" },
+    "LBE-C05": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-I01": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    // =====================================================
+    // Penalaran Matematika
+    // =====================================================
 
-    "PU-I02": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "PM-A01": { ringkasan:"", tips:"", contoh:"" },
+    "PM-A02": { ringkasan:"", tips:"", contoh:"" },
+    "PM-A03": { ringkasan:"", tips:"", contoh:"" },
+    "PM-A04": { ringkasan:"", tips:"", contoh:"" },
+    "PM-A05": { ringkasan:"", tips:"", contoh:"" },
+    "PM-A06": { ringkasan:"", tips:"", contoh:"" },
+    "PM-A07": { ringkasan:"", tips:"", contoh:"" },
+    "PM-A08": { ringkasan:"", tips:"", contoh:"" },
+    "PM-A09": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-I03": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "PM-B01": { ringkasan:"", tips:"", contoh:"" },
+    "PM-B02": { ringkasan:"", tips:"", contoh:"" },
+    "PM-B03": { ringkasan:"", tips:"", contoh:"" },
+    "PM-B04": { ringkasan:"", tips:"", contoh:"" },
+    "PM-B05": { ringkasan:"", tips:"", contoh:"" },
+    "PM-B06": { ringkasan:"", tips:"", contoh:"" },
+    "PM-B07": { ringkasan:"", tips:"", contoh:"" },
+    "PM-B08": { ringkasan:"", tips:"", contoh:"" },
+    "PM-B09": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-I04": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "PM-C01": { ringkasan:"", tips:"", contoh:"" },
+    "PM-C02": { ringkasan:"", tips:"", contoh:"" },
+    "PM-C03": { ringkasan:"", tips:"", contoh:"" },
+    "PM-C04": { ringkasan:"", tips:"", contoh:"" },
+    "PM-C05": { ringkasan:"", tips:"", contoh:"" },
 
-    "PU-I05": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
+    "PM-D01": { ringkasan:"", tips:"", contoh:"" },
+    "PM-D02": { ringkasan:"", tips:"", contoh:"" },
+    "PM-D03": { ringkasan:"", tips:"", contoh:"" },
+    "PM-D04": { ringkasan:"", tips:"", contoh:"" },
+    "PM-D05": { ringkasan:"", tips:"", contoh:"" },
+    "PM-D06": { ringkasan:"", tips:"", contoh:"" }
 
-    "PU-I06": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
-
-    "PU-I07": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
-
-    "PU-I08": {
-        ringkasan: "",
-        tips: "",
-        contoh: ""
-    },
 };
